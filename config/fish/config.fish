1282164b1e94bedc6ca070633d8597b9a6d17e4f
Juan Ignacio Donoso <jidonoso@gmail.com>
2021-03-17 15:46:31 -0300

    feat(gcloud): gcloud to the path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -16,3 +16,7 @@ set fish_greeting
 
 # Add to the path trusted directory
 set PATH .git/safe/../../bin $PATH
+
+# The next line updates PATH for the Google Cloud SDK.
+# TODO: remove this when homebrew installation works
+if [ -f '/opt/google-cloud-sdk/path.fish.inc' ]; . '/opt/google-cloud-sdk/path.fish.inc'; end
