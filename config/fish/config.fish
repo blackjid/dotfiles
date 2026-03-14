23af0c73edc9dcfdcc1821a61614cc3fe0d45227
Juan Ignacio Donoso <jidonoso@gmail.com>
2026-03-14 13:15:11 -0300

    feat(fish): swap done plugin for fish-eza and add EZA_STANDARD_OPTIONS

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -14,9 +14,5 @@ source /opt/homebrew/opt/asdf/libexec/asdf.fish
 # Remove greeting
 set fish_greeting
 
-# Add to the path trusted directory
-set PATH .git/safe/../../bin $PATH
-
-# The next line updates PATH for the Google Cloud SDK.
-# TODO: remove this when homebrew installation works
-if [ -f '/opt/google-cloud-sdk/path.fish.inc' ]; . '/opt/google-cloud-sdk/path.fish.inc'; end
+# Eza config
+set -q EZA_STANDARD_OPTIONS; or set -Ux EZA_STANDARD_OPTIONS --icons
